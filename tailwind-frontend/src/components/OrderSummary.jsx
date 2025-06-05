0945ee7c15f8a8f765f733d385f60ec8bfaa8225
Ola <hollabodey.kilaso@gmail.com>
2025-06-05 12:53:47 +0100

    fix: correct variable name for VeryPaleBlue and enhance cardColor styles in CSS

--- tailwind-frontend/src/components/OrderSummary.jsx
+++ tailwind-frontend/src/components/OrderSummary.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import dancer from "../assets/orderAssets/illustration-hero.svg";
 
 const OrderSummary = () => {
   return (
     <div className='orderBody h-screen flex items-center justify-center'>
-        <div className='rounded-3xl w-50 h-40 bg-amber-600 p-30'>
-
+        <div className='rounded-3xl w-50 h-40 bg-amber-600 p-30 cardColor'>
+          <img src={dancer} alt="image svg" />
         </div>
 
     </div>
