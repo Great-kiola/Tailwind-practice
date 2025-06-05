f6499f87a71e5ec7bd7ad2e7d74d0fbe01895769
Ola <hollabodey.kilaso@gmail.com>
2025-06-05 12:48:04 +0100

    refactor: enhance OrderSummary layout with flexbox for better alignment

--- tailwind-frontend/src/components/OrderSummary.jsx
+++ tailwind-frontend/src/components/OrderSummary.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const OrderSummary = () => {
   return (
-    <div className='orderBody h-screen'>
-        <div className='cardbtn'>
+    <div className='orderBody h-screen flex items-center justify-center'>
+        <div className='rounded-3xl w-50 h-40 bg-amber-600 p-30'>
 
         </div>
 
