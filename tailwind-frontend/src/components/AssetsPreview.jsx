72612265eeb2e1cb645db610b3e42ddc1e92d208
Ola <hollabodey.kilaso@gmail.com>
2025-06-12 21:58:58 +0100

    style: refactor AssetsPreview component to use fragment and improve structure

--- tailwind-frontend/src/components/AssetsPreview.jsx
+++ tailwind-frontend/src/components/AssetsPreview.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const AssetsPreview = () => {
-  return (
+  return <>
     <div>AssetsPreview</div>
-  )
+    <h1>Frontend mentor practice challenge</h1>
+  </>
 }
 
 export default AssetsPreview
