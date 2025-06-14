028b5022cdea966c73baa8b9794a49c8c842b9f8
Ola <hollabodey.kilaso@gmail.com>
2025-06-14 22:16:54 +0100

    style: add font styling to the heading in AssetsPreview component

--- tailwind-frontend/src/components/AssetsPreview.jsx
+++ tailwind-frontend/src/components/AssetsPreview.jsx
@@ -5,7 +5,7 @@ const AssetsPreview = () => {
     <div className='bg-Brightblue h-screen flex items-center justify-center'>
 
       <div className='w-120 bg-amber-200 p-10 text-amber-600 flex justify-center rounded-xl h-150'>
-        <h1 className=''>Frontend mentor practice challenge</h1>
+        <h1 className='font-bold text-xl'>Frontend mentor practice challenge</h1>
       </div>
 
     </div>
