539dd4f12b2a392bace998cef455fbce41fab5df
Ola <hollabodey.kilaso@gmail.com>
2025-06-15 23:46:14 +0100

    style: update AssetsPreview component to remove duplicate elements and improve structure

--- tailwind-frontend/src/components/AssetsPreview.jsx
+++ tailwind-frontend/src/components/AssetsPreview.jsx
@@ -4,10 +4,16 @@ const AssetsPreview = () => {
   return <>
     <div className='bg-Brightblue h-screen flex items-center justify-center'>
 
+
       <div className='w-120 bg-amber-200 p-10 text-amber-600 flex justify-center rounded-xl h-150'>
         <h1 className='font-bold text-xl'>Frontend mentor practice challenge</h1>
       </div>
-
+      <div className='w-120 bg-amber-200 p-10 text-amber-600 flex justify-center rounded-xl h-150'>
+        <h1 className='font-bold text-xl'>Frontend mentor practice challenge</h1>
+      </div>
+      <div className='w-120 bg-amber-200 p-10 text-amber-600 flex justify-center rounded-xl h-150'>
+        <h1 className='font-bold text-xl'>Frontend mentor practice challenge</h1>
+      </div>
     </div>
   </>
 }
