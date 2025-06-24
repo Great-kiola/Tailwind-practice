9a96a4d8c6d7648c0e3be70a588c9ca8b36f8609
Ola <hollabodey.kilaso@gmail.com>
2025-06-24 16:42:35 +0100

    style: reorganize heading elements in AssetsPreview component for improved structure

--- tailwind-frontend/src/components/AssetsPreview.jsx
+++ tailwind-frontend/src/components/AssetsPreview.jsx
@@ -7,11 +7,13 @@ const AssetsPreview = () => {
       <div className='w-120 bg-amber-200 p-10 text-amber-600 flex justify-center rounded-xl h-150'>
         <h1 className='font-bold text-xl'>Frontend mentor practice challenge</h1>
       </div>
+    </div>
 
-      <h1>Standard element</h1>
-      <h2>Standard element</h2>
-      <h3>Standard element</h3>
-      <h4>Standard element</h4>
+    <div>
+      <h2>Element 2</h2>
+      <h2>Element 3</h2>
+      <h2>Element 4</h2>
+      <h2>Element 5</h2>
     </div>
   </>
 }
