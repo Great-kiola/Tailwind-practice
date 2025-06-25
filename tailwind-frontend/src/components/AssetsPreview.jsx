f4e739cc91fb3ee2f39d3aaed751855fa6cede88
Ola <hollabodey.kilaso@gmail.com>
2025-06-25 21:44:33 +0100

    style: add missing heading elements in AssetsPreview component for improved structure

--- tailwind-frontend/src/components/AssetsPreview.jsx
+++ tailwind-frontend/src/components/AssetsPreview.jsx
@@ -15,6 +15,12 @@ const AssetsPreview = () => {
       <h2>Element 4</h2>
       <h2>Element 5</h2>
     </div>
+
+    <div>
+      <h1>Element</h1>
+      <h2>Of surprise</h2>
+      <h3>that includes everything that play around</h3>
+    </div>
   </>
 }
 
