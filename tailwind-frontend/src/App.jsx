a492debe1d6cf8589f814baabfc7a391e96d8aed
Ola <hollabodey.kilaso@gmail.com>
2025-07-01 15:51:17 +0100

    feat: implement Productpreview component and update App to use it

--- tailwind-frontend/src/App.jsx
+++ tailwind-frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 // import Nftpreview from "./components/Nftpreview"
 import OrderSummary from './components/OrderSummary'
 import RatingComponent from "./components/RatingComponent"
+import Productpreview from './components/Productpreview'
 
 
 const App = () => {
   return (
     <div>
-      <RatingComponent />
+      <Productpreview />
+      {/* <RatingComponent /> */}
       {/* <OrderSummary /> */}
       {/* <Nftpreview /> */}
     </div>
---
+++ tailwind-frontend/src/components/Productpreview.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Productpreview = () => {
+  return (
+    <div>Productpreview</div>
+  )
+}
+
+export default Productpreview
