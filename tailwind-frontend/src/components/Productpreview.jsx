cdbe19ea70ce8e351e2564a4cd800f31f357e765
Ola <hollabodey.kilaso@gmail.com>
2025-07-02 21:49:59 +0100

    style: update layout and spacing in Productpreview component for improved aesthetics

--- tailwind-frontend/src/components/Productpreview.jsx
+++ tailwind-frontend/src/components/Productpreview.jsx
@@ -5,29 +5,29 @@ import { FaBasketShopping } from "react-icons/fa6";
 
 const Productpreview = () => {
     return (
-        <div className='bg-cream h-screen flex items-center justify-center'>
+        <div className='bg-cream h-screen flex items-center justify-center lg:bg-cream'>
 
-            <div className='flex justify-center items-center w-200 h-150 bg-white rounded-3xl'>
+            <div className='flex justify-center items-center w-200 h-150 bg-white rounded-2xl'>
 
-                <div className='size-full'>
+                <div className='h-full w-320'>
                     <img className='rounded-l-3xl h-full' src={prodImage} alt="" />
                 </div>
 
                 <div className='p-10 h-full'>
                     <p className='uppercase tracking-[0.5rem]'>Perfume</p>
-                    <h1 className='text-5xl font-fraunces font-extrabold'>Gabrielle Essence Eau De Parfum</h1>
+                    <h1 className='text-5xl font-fraunces font-extrabold my-7'>Gabrielle Essence Eau De Parfum</h1>
 
                     <p className='text-prod-grey my-5 text-lg'>
                         A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.
                     </p>
 
-                    <div className='flex gap-5 items-center '>
+                    <div className='flex gap-5 items-center my-10'>
                         <h1 className='font-fraunces text-5xl font-bold text-Green-light'>$149.99</h1>
                         <span className='line-through text-lg'>$169.99</span>
 
                     </div>
 
-                    <button className='bg-Green-light w-full text-white h-15 rounded-lg font-montserrat flex items-center justify-center gap-3 text-lg font-bold mt-10'>
+                    <button className='bg-Green-light w-full text-white h-15 rounded-lg font-montserrat flex items-center justify-center gap-3 text-lg font-bold mt-10 hover:bg-Green-dark'>
                         <FaBasketShopping/>
                         Add to Cart
                     </button>
