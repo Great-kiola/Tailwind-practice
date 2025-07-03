181e08e28dde2f8d6e59e525773d51cb6e6881e9
Ola <hollabodey.kilaso@gmail.com>
2025-07-04 00:57:59 +0100

    feat: update SocialLinks component layout and styles for improved presentation

--- tailwind-frontend/src/components/SocialLinks.jsx
+++ tailwind-frontend/src/components/SocialLinks.jsx
@@ -5,20 +5,28 @@ const SocialLinks = () => {
   return (
 
     <section className='bg-Grey-900 h-screen flex items-center justify-center'>
-        <div className='bg-Grey-800 w-100 flex items-center justify-center rounded-xl'>
-          <div className='grid place-items-center'>
+      <div className='bg-Grey-800 w-100 flex items-center justify-center rounded-xl'>
+        <div className='grid place-items-center w-full'>
 
-            <img src={jessica} alt="" className='rounded-full w-20 my-5' />
-            <h1 className='text-xl text-WhiteLinks'>Jessica Randall</h1>
-            <p className='text-greenLinks'>London, United Kingdom</p>
+          <img src={jessica} alt="" className='rounded-full w-20 my-9' />
+          <h1 className='text-xl text-WhiteLinks'>Jessica Randall</h1>
+          <p className='text-greenLinks'>London, United Kingdom</p>
 
-            <p className='text-gray-500 my-5'>"Front-end developer and avid reader"</p>
+          <p className='text-gray-300 my-5 text-sm'>"Front-end developer and avid reader"</p>
 
 
-            <h1>Github</h1>
+          <div className='grid place-content-center mb-5'>
+            <a href="#" target='_blank' className='links '>Github</a>
+            <a href="#" target='_blank' className='links'>Frontend Mentor</a>
+            <a href="#" target='_blank' className='links'>LinkedIn</a>
+            <a href="#" target='_blank' className='links'>Twitter</a>
+            <a href="#" target='_blank' className='links'>Instagram</a>
           </div>
 
+
         </div>
+
+      </div>
 
     </section>
   )
