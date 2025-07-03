1c3369b0ac812cc4df623e782a6072826024d041
Ola <hollabodey.kilaso@gmail.com>
2025-07-04 00:40:09 +0100

    feat: add avatar image and update styles for SocialLinks component

--- tailwind-frontend/src/components/SocialLinks.jsx
+++ tailwind-frontend/src/components/SocialLinks.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import jessica from "../assets/SocialLinks/avatar-jessica.jpeg"
 
 const SocialLinks = () => {
   return (
 
-    <section className='bg-white h-screen flex items-center justify-center'>
-        <div className='h-200 bg-Green-light '>
-            <div className='bg-blue-500 w-300 h-200'>SocialLinks</div>
+    <section className='bg-Grey-900 h-screen flex items-center justify-center'>
+        <div className='bg-Grey-800 w-100 flex items-center justify-center rounded-xl'>
+          <div className='grid place-items-center'>
 
+            <img src={jessica} alt="" className='rounded-full w-20 my-5' />
+            <h1 className='text-xl text-WhiteLinks'>Jessica Randall</h1>
+            <p className='text-greenLinks'>London, United Kingdom</p>
+          </div>
         </div>
 
     </section>
