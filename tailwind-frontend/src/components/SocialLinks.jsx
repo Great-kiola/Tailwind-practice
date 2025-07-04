0a2322711df0530c43390db5b1ee405af441bbb4
Ola <hollabodey.kilaso@gmail.com>
2025-07-04 01:25:08 +0100

    feat: update SocialLinks component with improved layout and button styles

--- tailwind-frontend/src/components/SocialLinks.jsx
+++ tailwind-frontend/src/components/SocialLinks.jsx
@@ -9,18 +9,40 @@ const SocialLinks = () => {
         <div className='grid place-items-center w-full'>
 
           <img src={jessica} alt="" className='rounded-full w-20 my-9' />
-          <h1 className='text-xl text-WhiteLinks'>Jessica Randall</h1>
+          <h1 className='text-3xl text-WhiteLinks'>Jessica Randall</h1>
           <p className='text-greenLinks'>London, United Kingdom</p>
 
           <p className='text-gray-300 my-5 text-sm'>"Front-end developer and avid reader"</p>
 
 
           <div className='grid place-content-center mb-5'>
-            <a href="#" target='_blank' className='links '>Github</a>
-            <a href="#" target='_blank' className='links'>Frontend Mentor</a>
-            <a href="#" target='_blank' className='links'>LinkedIn</a>
-            <a href="#" target='_blank' className='links'>Twitter</a>
-            <a href="#" target='_blank' className='links'>Instagram</a>
+
+
+            <button class="relative overflow-hidden font-medium transition-all bg-Grey-700 rounded group h-10">
+              <span class="buttonSpan"></span>
+              <a href="https://x.com" target='_blank' className='links'>Github</a>
+            </button>
+
+            <button class="relative overflow-hidden font-medium transition-all bg-Grey-700 rounded group h-10">
+              <span class="buttonSpan"></span>
+              <a href="https://x.com" target='_blank' className='links'>Frontend Mentor</a>
+            </button>
+
+            <button class="relative overflow-hidden font-medium transition-all bg-Grey-700 rounded group h-10">
+              <span class="buttonSpan"></span>
+              <a href="https://x.com" target='_blank' className='links'>LinkedIn</a>
+            </button>
+
+            <button class="relative overflow-hidden font-medium transition-all bg-Grey-700 rounded group h-10">
+              <span class="buttonSpan"></span>
+              <a href="https://x.com" target='_blank' className='links'>Twitter</a>
+            </button>
+
+            <button class="relative overflow-hidden font-medium transition-all bg-Grey-700 rounded group h-10">
+              <span class="buttonSpan"></span>
+              <a href="https://x.com" target='_blank' className="relative w-full text-left text-indigo-600 transition-colors duration-300 ease-in-out group-hover:text-white">Instagram</a>
+            </button>
+
           </div>
 
 
