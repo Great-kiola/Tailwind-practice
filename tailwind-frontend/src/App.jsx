fdb39433db75e36fbe57ade6ad522c29d165c9d8
Ola <hollabodey.kilaso@gmail.com>
2025-07-07 23:51:36 +0100

    feat: add react-router-dom and cookie dependencies to package-lock.json

--- tailwind-frontend/src/App.jsx
+++ tailwind-frontend/src/App.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-// import Nftpreview from "./components/Nftpreview"
+import Nftpreview from "./components/Nftpreview"
 import OrderSummary from './components/OrderSummary'
 import RatingComponent from "./components/RatingComponent"
 import Productpreview from './components/Productpreview'
 import SocialLinks from "./components/SocialLinks"
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <SocialLinks />
-      {/* <Productpreview /> */}
-      {/* <RatingComponent /> */}
-      {/* <OrderSummary /> */}
-      {/* <Nftpreview /> */}
+
+      <Routes >
+        <Route path="/" element= {<OrderSummary />} />
+        <Route path='/nftPreview' element= {<Nftpreview />} />
+        <Route path='/ratingComponent' element= {<RatingComponent />} />
+        <Route path= "/productPreview" element={ <Productpreview />}  />
+        <Route path= "/socialLinks" element={ <SocialLinks />}  />
+
+      </Routes>
     </div>
   )
 }
--- tailwind-frontend/src/main.jsx
+++ tailwind-frontend/src/main.jsx
@@ -2,11 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+import {BrowserRouter} from "react-router-dom"
 // import Nftpreview from "./components/Nftpreview"
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    
   </StrictMode>,
 )
