01cf4928330c3c33d87a7b3e8483356806b9de8e
Ola <hollabodey.kilaso@gmail.com>
2025-07-08 16:19:32 +0100

    feat: implement ResultSummary component layout and styling

--- tailwind-frontend/src/components/ResultSummary.jsx
+++ tailwind-frontend/src/components/ResultSummary.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 const ResultSummary = () => {
   return (
-    <div>ResultSummary</div>
-  )
+    <>
+        <div className='bg-Pale-blue h-screen flex items-center justify-center'>
+            <div className='h-150 w-200 rounded-4xl bg-White-resultSum'>
+                <div>
+                    <h2>Your result</h2>
+
+                    <div className='rounded-full'>
+                        <h1>76</h1>
+                        <p>of 100</p>
+                    </div>
+
+                    <h1>Great</h1>
+                    <h2>You scored higher than 65% of the people who have taken these tests.</h2>
+                </div>
+                <div></div>
+            </div>
+        </div>
+    </>
+)
 }
 
 export default ResultSummary
