d1206ebdf7d7ec58ef83acd1a1f9eac9ef85eb7a
Ola <hollabodey.kilaso@gmail.com>
2025-07-09 22:09:06 +0100

    feat: enhance ResultSummary component with icons and summary layout

--- tailwind-frontend/src/components/ResultSummary.jsx
+++ tailwind-frontend/src/components/ResultSummary.jsx
@@ -1,26 +1,53 @@
 import React from 'react'
+import { HiOutlineLightningBolt } from "react-icons/hi";
+import { RiBrainLine } from "react-icons/ri";
+
+
 
 const ResultSummary = () => {
-  return (
-    <>
-        <div className='bg-Pale-blue h-screen flex items-center justify-center'>
-            <div className='h-150 w-200 rounded-4xl bg-White-resultSum'>
-                <div>
-                    <h2>Your result</h2>
+    return (
+        <>
+            <div className='bg-Pale-blue h-screen flex items-center justify-center'>
+                <div className='h-150 w-200 rounded-4xl bg-White-resultSum'>
+                    <div className=''>
+                        <h2>Your result</h2>
 
-                    <div className='rounded-full'>
-                        <h1>76</h1>
-                        <p>of 100</p>
+                        <div className='rounded-full'>
+                            <h1>76</h1>
+                            <p>of 100</p>
+                        </div>
+
+                        <h1>Great</h1>
+                        <h2>You scored higher than 65% of the people who have taken these tests.</h2>
                     </div>
+                    <div>
+                        <h1 className=''>Summary</h1>
 
-                    <h1>Great</h1>
-                    <h2>You scored higher than 65% of the people who have taken these tests.</h2>
+                        <div className='Summaries'>
+                            <div className='bg-red-500'>
+                                <div>
+                                    <HiOutlineLightningBolt />
+                                    <h1>Reaction</h1>
+                                </div>
+
+                                <h1><span>80</span> / 100</h1>
+                            </div>
+
+                            <div className='bg-orange-500'>
+                                <div>
+                                    <RiBrainLine />
+
+                                    <h1>Memory</h1>
+                                </div>
+
+                                <h1><span>92</span> / 100</h1>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div></div>
             </div>
-        </div>
-    </>
-)
+        </>
+    )
 }
 
 export default ResultSummary
