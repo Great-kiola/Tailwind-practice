c3d267b879b22d0fd9a19bd5877179852761c2e5
Ola <hollabodey.kilaso@gmail.com>
2025-07-12 15:34:12 +0100

    chore: clean up unused imports and comments in ResultSummary component

--- tailwind-frontend/src/components/ResultSummary.jsx
+++ tailwind-frontend/src/components/ResultSummary.jsx
@@ -8,24 +8,24 @@ const ResultSummary = () => {
     return (
         <>
             <div className='bg-Pale-blue h-screen flex items-center justify-center'>
-                <div className='h-150 w-200 rounded-4xl bg-White-resultSum flex '>
-                    <div className='bg-linear-to-t from-Light-royal-blue to-Light-slate-blue rounded-4xl flex items-center basis-sm'>
-                        <h2>Your result</h2>
+                <div className='h-150 w-170 rounded-4xl bg-White-resultSum flex '>
+                    <div className='bg-linear-to-t from-Light-royal-blue to-Light-slate-blue rounded-4xl p-10'>
+                        <h2 className='text-Light-lavender font-bold grid place-content-center text-2xl mb-10'>Your result</h2>
 
-                        <div className='rounded-full bg-linear-to-t from-Persian-blue to-Violet-blue'>
-                            <h1 className='text-White-resultSum font-bold text-4xl'>76</h1>
-                            <p>of 100</p>
+                        <div className='rounded-full bg-linear-to-t from-Persian-blue to-Violet-blue size-50 grid place-content-center'>
+                            <h1 className='text-White-resultSum font-bold text-6xl'>76</h1>
+                            <p className='flex justify-center'>of 100</p>
                         </div>
 
-                        <h1>Great</h1>
+                        <h1 className=''>Great</h1>
                         <h2>You scored higher than 65% of the people who have taken these tests.</h2>
                     </div>
-                    <div>
-                        <h1 className=''>Summary</h1>
+                    <div className='p-10'>
+                        <h1 className='text-xl font-bold'>Summary</h1>
 
                         <div className='Summaries'>
-                            <div className='bg-red-500'>
-                                <div>
+                            <div className='bg-red-500 summaries '>
+                                <div className='flex items-center'>
                                     <HiOutlineLightningBolt />
                                     <h1>Reaction</h1>
                                 </div>
@@ -33,7 +33,7 @@ const ResultSummary = () => {
                                 <h1><span>80</span> / 100</h1>
                             </div>
 
-                            <div className='bg-orange-500'>
+                            <div className='bg-orange-500 summaries'>
                                 <div>
                                     <RiBrainLine />
 
