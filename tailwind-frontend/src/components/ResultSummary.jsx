93a52abc1b02dde0bf92849d94057d5db14dffee
Ola <hollabodey.kilaso@gmail.com>
2025-07-12 23:15:24 +0100

    chore: refine ResultSummary component layout and styling for improved consistency

--- tailwind-frontend/src/components/ResultSummary.jsx
+++ tailwind-frontend/src/components/ResultSummary.jsx
@@ -8,10 +8,10 @@ const ResultSummary = () => {
     return (
         <>
             <div className='bg-Pale-blue h-screen flex items-center justify-center font-hanken'>
-                <div className='h-130 w-180 rounded-4xl bg-White-resultSum flex '>
+                <div className='w-180 rounded-4xl bg-White-resultSum flex '>
 
                     <div className='bg-linear-to-t from-Light-royal-blue to-Light-slate-blue rounded-4xl p-10 w-1/2 flex items-center justify-between flex-col'>
-                        <h2 className='text-Light-lavender font-bold grid place-content-center text-2xl '>Your result</h2>
+                        <h2 className='text-Light-lavender font-bold grid place-content-center text-xl'>Your result</h2>
 
                         <div className='rounded-full bg-linear-to-t from-Persian-blue to-Violet-blue size-50 grid place-content-center'>
                             <h1 className='text-White-resultSum font-bold text-6xl font-hanken'>76</h1>
@@ -19,7 +19,7 @@ const ResultSummary = () => {
                         </div>
 
                         <h1 className='text-white grid place-content-center text-3xl font-bold font-hanken'>Great</h1>
-                        <h2 className='text-Light-lavender text-xl '>You scored higher than 65% of the people who have taken these tests.</h2>
+                        <h2 className='text-Light-lavender text-md text-center px-7'>You scored higher than 65% of the people who have taken these tests.</h2>
                     </div>
 
                     <div className='p-10'>
@@ -35,34 +35,36 @@ const ResultSummary = () => {
                                 <h1 ><span className='font-bold'>80</span> / 100</h1>
                             </div>
 
-                            <div className='bg-Orangey-yellow summaries'>
+                            <div className='summaries'>
                                 <div className='flex items-center gap-2'>
-                                    <RiBrainLine className='text-xl' />
+                                    <RiBrainLine className='text-xl text-Orangey-yellow' />
 
-                                    <h1 className='font-bold'>Memory</h1>
+                                    <h1 className='font-bold text-Orangey-yellow'>Memory</h1>
                                 </div>
 
                                 <h1><span className='font-bold'>92</span> / 100</h1>
                             </div>
 
-                            <div className='bg-Pale-blue summaries '>
+                            <div className='summaries '>
                                 <div className='flex items-center gap-2'>
-                                    <HiOutlineLightningBolt className='text-xl text-Light-red' />
-                                    <h1 className='font-bold text-Light-red'>Reaction</h1>
+                                    <HiOutlineLightningBolt className='text-xl text-Green-teal' />
+                                    <h1 className='font-bold text-Green-teal'>Verbal</h1>
                                 </div>
 
                                 <h1 ><span className='font-bold'>80</span> / 100</h1>
                             </div>
 
-                            <div className='bg-Orangey-yellow summaries'>
+                            <div className='summaries'>
                                 <div className='flex items-center gap-2'>
-                                    <RiBrainLine className='text-xl' />
+                                    <RiBrainLine className='text-xl text-Cobalt-blue' />
 
-                                    <h1 className='font-bold'>Memory</h1>
+                                    <h1 className='font-bold text-Cobalt-blue'>Visual</h1>
                                 </div>
 
                                 <h1><span className='font-bold'>92</span> / 100</h1>
                             </div>
+
+                            <button className='bg-Dark-gray-blue p-4 rounded-full w-full text-white my-5'>Continue</button>
                         </div>
                     </div>
 
