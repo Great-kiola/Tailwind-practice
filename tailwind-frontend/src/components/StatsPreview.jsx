a13b15cbf90ac8637ed546ab56c1e85004e4ce6b
Ola <hollabodey.kilaso@gmail.com>
2025-07-13 02:42:35 +0100

    chore: add header image and enhance StatsPreview component layout and styling

--- tailwind-frontend/src/components/StatsPreview.jsx
+++ tailwind-frontend/src/components/StatsPreview.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import employees from "../assets/StatsPreview/header-desktop.jpg"
 
 const StatsPreview = () => {
-  return (
-    <>
-        <div className='bg-veryDarkBlue h-screen'></div>
-    </>
-  )
+    return (
+        <>
+            <div className='bg-veryDarkBlue h-screen flex items-center justify-center'>
+
+                <div className='cardStats bg-desatBlue w-250 flex items-center'>
+                    <div>
+                        <h1 className='text-lg'>Get <span className='text-softViolet'>Insights</span> That help your business grow.</h1>
+                        <p>Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.</p>
+
+                        <div>
+                            <div>
+                                <h1>10k+</h1>
+                                <p>COMPANIES</p>
+                            </div>
+                            <div>
+                                <h1>314</h1>
+                                <p>TEMPLATES</p>
+                            </div>
+                            <div>
+                                <h1>12M+</h1>
+                                <p>QUERIES</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <img src={employees} alt="" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default StatsPreview
