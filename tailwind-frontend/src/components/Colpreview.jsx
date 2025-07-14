648fec0304c63b3593f5075aa54d1902727e7694
Ola <hollabodey.kilaso@gmail.com>
2025-07-14 22:15:35 +0100

    chore: implement Colpreview component layout with structured divs and headings

--- tailwind-frontend/src/components/Colpreview.jsx
+++ tailwind-frontend/src/components/Colpreview.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Colpreview = () => {
   return (
-    <div>Colpreview</div>
+    <>
+      <div className='bg-div'>
+        <div>
+          <h1>First Div</h1>
+        </div>
+
+        <div>
+          <h2>Second Div</h2>
+        </div>
+        
+        <div>
+          <div>zz</div>
+        </div>
+      </div>
+    </>
   )
 }
 
