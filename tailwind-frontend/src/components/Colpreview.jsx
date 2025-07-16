ddbecd65c68e957e67444619b5d7b400b6e5ea33
Ola <hollabodey.kilaso@gmail.com>
2025-07-16 21:22:01 +0100

    chore: update Colpreview component layout and add new color variables in CSS

--- tailwind-frontend/src/components/Colpreview.jsx
+++ tailwind-frontend/src/components/Colpreview.jsx
@@ -3,14 +3,16 @@ import React from 'react'
 const Colpreview = () => {
   return (
     <>
-      <div className='bg-div'>
-        
-        <div>
-          <img src="icon" alt="car icon" />
-          <h1>Sedans</h1>
-          <p>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip</p>
+      <div className='bg-Gray-100 h-screen'>
 
-          <button>Learn More</button>
+        <div>
+          <div>
+            <img src="icon" alt="car icon" />
+            <h1>Sedans</h1>
+            <p>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip</p>
+
+            <button>Learn More</button>
+          </div>
         </div>
 
       </div>
