19b9c03d13b27bb869214d226f7e325ce92363f9
Ola <hollabodey.kilaso@gmail.com>
2025-07-16 21:36:58 +0100

    chore: enhance Colpreview component layout with flexbox and new card styles

--- tailwind-frontend/src/components/Colpreview.jsx
+++ tailwind-frontend/src/components/Colpreview.jsx
@@ -3,15 +3,32 @@ import React from 'react'
 const Colpreview = () => {
   return (
     <>
-      <div className='bg-Gray-100 h-screen'>
+      <div className='bg-Gray-100 h-screen flex justify-center'>
 
-        <div>
-          <div>
+        <div className='flex w-200 items-center '>
+
+          <div className='bg-Gold-500 cards rounded-l-xl flex flex-col'>
+            <img src="icon" alt="car icon" />
+            <h1 className='text-3xl my-5'>Sedans</h1>
+            <p>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip</p>
+
+            <button className='bg-Transparent-white py-3 rounded-full'>Learn More</button>
+          </div>
+
+          <div className='bg-Cyan-800 cards'>
             <img src="icon" alt="car icon" />
             <h1>Sedans</h1>
             <p>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip</p>
 
             <button>Learn More</button>
+          </div>
+
+          <div className='bg-Green-950 cards rounded-r-xl'>
+            <img src="icon" alt="car icon" />
+            <h1>Sedans</h1>
+            <p>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip</p>
+
+            <button className=''>Learn More</button>
           </div>
         </div>
 
