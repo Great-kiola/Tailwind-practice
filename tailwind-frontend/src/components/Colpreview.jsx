d59337ebd04342a38bf09d41352fc2e36791e6fd
Ola <hollabodey.kilaso@gmail.com>
2025-07-20 21:56:12 +0100

    chore: improve layout of Gold card in Colpreview component for better structure and spacing

--- tailwind-frontend/src/components/Colpreview.jsx
+++ tailwind-frontend/src/components/Colpreview.jsx
@@ -7,12 +7,16 @@ const Colpreview = () => {
 
         <div className='flex w-200 items-center '>
 
-          <div className='bg-Gold-500 cards rounded-l-xl flex flex-col'>
-            <img src="icon" alt="car icon" />
-            <h1 className='text-3xl my-5'>Sedans</h1>
-            <p>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip</p>
+          <div className='bg-Gold-500 cards rounded-l-xl flex flex-col justify-between'>
+            <div>
+              <img src="icon" alt="car icon" />
+              <h1 className='text-3xl my-5'>Sedans</h1>
+              <p>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip</p>
+            </div>
 
-            <button className='bg-Transparent-white py-3 rounded-full'>Learn More</button>
+            <div classna>
+              <button className='bg-Transparent-white py-3 rounded-full w-full'>Learn More</button>
+            </div>
           </div>
 
           <div className='bg-Cyan-800 cards'>
