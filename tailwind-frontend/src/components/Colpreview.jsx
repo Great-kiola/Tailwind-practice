622d086a6151840c28b6bc312f87494b5a7cbbeb
Ola <hollabodey.kilaso@gmail.com>
2025-07-23 04:20:55 +0100

    chore: add new car icons and update Colpreview component for improved visuals and content

--- tailwind-frontend/src/components/Colpreview.jsx
+++ tailwind-frontend/src/components/Colpreview.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import luxuryIcon from "../assets/Colpreview/icon-luxury.svg"
+import sedans from "../assets/Colpreview/icon-sedans.svg"
+import suvs from "../assets/Colpreview/icon-suvs.svg"
 
 const Colpreview = () => {
   return (
@@ -9,37 +12,37 @@ const Colpreview = () => {
 
           <div className='bg-Gold-500 cards rounded-l-xl flex flex-col justify-between w-280'>
             <div>
-              <img src="icon" alt="car icon" />
+              <img src={sedans} alt="car icon" />
               <h1 className='text-3xl my-5 font-BigShoulders uppercase text-white'>Sedans</h1>
-              <p className='pr-10 text-white text-sm'>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip</p>
+              <p className='pr-10 text-Transparent-white text-[13px]'>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
             </div>
 
             <div >
-              <button className='bg-white py-3 rounded-full w-full hover:text-white hover:bg-transparent text-Gold-500 border-2'>Learn More</button>
+              <button className='bg-white py-2 rounded-full px-6 text-sm hover:text-white hover:bg-transparent text-Gold-500 border-2'>Learn More</button>
             </div>
           </div>
 
           <div className='bg-Cyan-800 cards flex flex-col justify-between w-280'>
             <div>
-              <img src="icon" alt="car icon" />
+              <img src={suvs} alt="car icon" />
               <h1 className='text-3xl my-5 font-BigShoulders uppercase text-white'>SUVS</h1>
-              <p className='pr-10 text-sm text-white'>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip</p>
+              <p className='pr-10 text-sm text-Transparent-white text-[13px]'>Take an SUV for its spacious interior,power, and versatility. Perfect for your next family vacations and off-road adventures.</p>
             </div>
 
             <div >
-              <button className='bg-white py-3 rounded-full w-full hover:text-white hover:bg-transparent text-Cyan-800 border-2'>Learn More</button>
+              <button className='bg-white py-2 rounded-full px-6 text-sm hover:text-white hover:bg-transparent text-Cyan-800 border-2'>Learn More</button>
             </div>
           </div>
 
           <div className='bg-Green-950 cards rounded-r-xl flex flex-col justify-between w-280'>
             <div>
-              <img src="icon" alt="car icon" />
+              <img src={luxuryIcon} alt="car icon" />
               <h1 className='text-3xl my-5 font-BigShoulders uppercase text-white'>Luxury</h1>
-              <p className='pr-10 text-sm text-white'>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip</p>
+              <p className='pr-10 text-sm text-Transparent-white text-[13px]'>Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.</p>
             </div>
 
             <div >
-              <button className='bg-white py-3 rounded-full w-full hover:text-white hover:bg-transparent text-Green-950 border-2'>Learn More</button>
+              <button className='bg-white py-2 text-sm rounded-full px-6 hover:text-white hover:bg-transparent text-Green-950 border-2'>Learn More</button>
             </div>
           </div>
         </div>
