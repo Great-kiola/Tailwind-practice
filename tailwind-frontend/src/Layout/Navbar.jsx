dd4e284f111b23d55721cf34993f490f2ff469f9
Ola <hollabodey.kilaso@gmail.com>
2025-07-31 23:01:27 +0100

    chore: comment out button and SideMenu rendering logic in Navbar component for future implementation

--- tailwind-frontend/src/Layout/Navbar.jsx
+++ tailwind-frontend/src/Layout/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({activeMenu}) => {
   const [openSideMenu, setOpenSideMenu ] = useState(false);
   return (
     <div className='flex gap-5 bg-white border border-b border-gray-200/50 backdrop-blur-[2px] py-4 px-7 sticky top-0 z-30'>
-      <button
+      {/* <button
         className='block lg:hidden text-black'
         onClick={() => {
           setOpenSideMenu(!openSideMenu);
@@ -25,7 +25,7 @@ const Navbar = ({activeMenu}) => {
         <div className='fixed top-[61px] ml-4 bg-white'>
           <SideMenu activeMenu={activeMenu} />
         </div>
-      )}
+      )} */}
     </div>
   )
 }
