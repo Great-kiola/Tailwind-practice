39bba2b91cb5b04ffe935b464ec21415ce3db5af
Ola <hollabodey.kilaso@gmail.com>
2025-08-15 22:43:42 +0300

    chore: update Navbar background color for better visibility

--- tailwind-frontend/src/Layout/Navbar.jsx
+++ tailwind-frontend/src/Layout/Navbar.jsx
@@ -5,7 +5,7 @@ import SideMenu from './SideMenu';
 const Navbar = ({activeMenu}) => {
   const [openSideMenu, setOpenSideMenu ] = useState(false);
   return (
-    <div className='flex gap-5 bg-white border border-b border-gray-200/50 backdrop-blur-[2px] py-4 px-7 sticky top-0 z-30'>
+    <div className='flex gap-5 bg-Grey-700 border border-b border-gray-200/50 backdrop-blur-[2px] py-4 px-7 sticky top-0 z-30'>
       <button
         className='block text-black'
         onClick={() => {
@@ -19,7 +19,7 @@ const Navbar = ({activeMenu}) => {
         )}
       </button>
 
-      <h2 className='text-lg text-black'>FrontEnd Mentor Challenges</h2>
+      <h2 className='text-lg text-black '>FrontEnd Mentor Challenges</h2>
 
       {openSideMenu && (
         <div className='fixed top-[61px] ml-4 bg-white'>
