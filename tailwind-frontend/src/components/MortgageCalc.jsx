1c639d65f855d6e4cad26a27a1226cbfa2932529
Ola <hollabodey.kilaso@gmail.com>
2025-08-30 19:28:21 +0100

    chore: enhance MortgageCalc layout and styling for better user experience

--- tailwind-frontend/src/components/MortgageCalc.jsx
+++ tailwind-frontend/src/components/MortgageCalc.jsx
@@ -5,8 +5,8 @@ import calculator from "../assets/MortgageCalc/images/icon-calculator.svg"
 const MortgageCalc = () => {
   return (
     <div className='bg-SlateMorgageCalc100 h-screen flex items-center justify-center'>
-      <div className='bg-WhiteMortgageCalc w-270 h-150 flex'>
-        <div className='w-200'>
+      <div className='bg-WhiteMortgageCalc w-270 h-155 flex rounded-4xl shadow-xl'>
+        <div className='w-full p-10'>
           <div className='title'>
             <h1>Mortgage Calculator</h1>
             <p><a href="#">Clear All</a></p>
@@ -14,18 +14,18 @@ const MortgageCalc = () => {
 
           <div className='formDiv'></div>
 
-          <button className='bg-LimeMortgageCalc flex gap-2 p-4 rounded-full'>
+          <button className='bg-LimeMortgageCalc flex gap-2 p-4 rounded-full font-bold'>
             <img src={calculator} alt="claculator icon" />
             Calculate Repayments
           </button>
 
         </div>
 
-        <div className='w-full bg-SlateMortgageCalc900 rounded-tr-4xl rounded-bl-[120px] flex justify-center items-center flex-col'>
+        <div className='w-230 bg-SlateMortgageCalc900 rounded-tr-4xl rounded-bl-[120px] flex justify-center items-center flex-col rounded-br-4xl'>
           <img src={illustratioon} alt="" className='w-50'/>
 
           <h1 className='text-WhiteMortgageCalc font-bold text-2xl'>Results shown here</h1>
-          <p className='text-SlateMorgageCalc300 text-center'>Complete the form and click "calculate repayments" to see what your monthly repayments would be. </p>
+          <p className='text-SlateMorgageCalc300 text-center text-md px-16 pt-3'>Complete the form and click "calculate repayments" to see what your monthly repayments would be. </p>
         </div>
       </div>
 
