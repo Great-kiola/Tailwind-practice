8cea40419dd02aa4b97c2ec59723b4138e2351ad
Ola <hollabodey.kilaso@gmail.com>
2025-08-30 23:33:29 +0100

    chore: enhance form structure and layout in MortgageCalc component for better usability

--- tailwind-frontend/src/components/MortgageCalc.jsx
+++ tailwind-frontend/src/components/MortgageCalc.jsx
@@ -6,28 +6,66 @@ const MortgageCalc = () => {
   return (
     <div className='bg-SlateMortgageCalc100 h-screen flex items-center justify-center font-jakarta'>
       <div className='bg-WhiteMortgageCalc w-270 h-155 flex rounded-4xl shadow-xl'>
-        <div className='w-full p-10'>
+        <div className='w-full p-10 flex flex-col justify-between'>
           <div className='flex justify-between mb-5'>
             <h1 className='font-bold text-xl '>Mortgage Calculator</h1>
             <p className='underline text-SlateMortgageCalc300'><a href="#">Clear All</a></p>
           </div>
 
-          <div className='formDiv mb-20'>
+          <div className='formDiv'>
             <form action="" method="get">
-              {/* Form */}
-              <label htmlFor="log-email" className="text-SlateMortgageCalc700">Mortgage Amount</label>
 
-              <div class="flex mt-2">
-                <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-SlateMortgageCalc100 border rounded-e-0 border-SlateMortgageCalc500 border-r-0 rounded-s-md">
+              <label htmlFor="log-email" className="text-SlateMortgageCalc700 font-medium text-sm">Mortgage Amount</label>
+              <div class="flex mt-2 mb-5">
+                <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-SlateMortgageCalc100 border rounded-e-0 border-SlateMortgageCalc300 border-r-0 rounded-s-md">
                   <span className='font-bold text-SlateMortgageCalc700'>£</span>
                 </span>
-                <input type="text" id="website-admin" className="rounded-none rounded-e-lg bg-gray-50 border border-l-0 text-gray-900  w-full text-sm border-SlateMortgageCalc500 p-2.5 h-12" />
+                <input type="text" id="website-admin" className="rounded-none rounded-e-lg bg-gray-50 border border-l-0 text-gray-900  w-full text-sm border-SlateMortgageCalc300 p-2.5 h-12" />
               </div>
+
+              <div className='flex gap-7 mb-5'>
+                <div>
+                  <label htmlFor="log-email" className="text-SlateMortgageCalc700 font-medium text-sm">Mortgage Term</label>
+                  <div class="flex mt-2">
+                    <input type="text" id="website-admin" className="rounded-l-lg bg-gray-50 border border-r-0 text-gray-900 text-sm border-SlateMortgageCalc300 p-2.5 h-12" />
+
+                    <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-SlateMortgageCalc100 border rounded-e-0 border-SlateMortgageCalc300 border-l-0 rounded-r-lg">
+                      <span className='font-bold text-SlateMortgageCalc700'>years</span>
+                    </span>
+                  </div>
+                </div>
+
+                <div>
+                  <label htmlFor="log-email" className="text-SlateMortgageCalc700 font-medium text-sm">Interest Rate</label>
+                  <div class="flex mt-2">
+                    <input type="text" id="website-admin" className="rounded-l-lg bg-gray-50 border border-r-0 text-gray-900 text-sm border-SlateMortgageCalc300 p-2.5 h-12" />
+
+                    <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-SlateMortgageCalc100 border rounded-e-0 border-SlateMortgageCalc300 border-l-0 rounded-r-lg">
+                      <span className='font-bold text-SlateMortgageCalc700'>%</span>
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <label htmlFor="" className='text-sm'>Mortgage Type</label>
+              <label className='border p-4 flex gap-4 mt-4 rounded-md items-center border-SlateMortgageCalc300'>
+                <input type="checkbox" name="" id="" className='size-5 ' />
+                <span className='font-bold'>Repayment</span>
+              </label>
+
+              <label className='border p-4 flex gap-4 mt-2 rounded-md items-center border-SlateMortgageCalc300'>
+                <input type="checkbox" name="" id="" className='size-5' />
+                <span className='font-bold'>Interest Only</span>
+              </label>
+
+
+
+
             </form>
 
           </div>
 
-          <button className='bg-LimeMortgageCalc flex gap-2 p-4 rounded-full font-bold'>
+          <button className='bg-LimeMortgageCalc flex gap-2 p-4 rounded-full font-bold w-75 pl-10'>
             <img src={calculator} alt="claculator icon" />
             Calculate Repayments
           </button>
