a041e89df9ad2438bb565ce368a8da49a9101ea5
Ola <hollabodey.kilaso@gmail.com>
2025-08-31 03:03:41 +0100

    chore: enhance input styling and improve results display in MortgageCalc component for better user experience

--- tailwind-frontend/src/components/MortgageCalc.jsx
+++ tailwind-frontend/src/components/MortgageCalc.jsx
@@ -20,14 +20,14 @@ const MortgageCalc = () => {
                 <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-SlateMortgageCalc100 border rounded-e-0 border-SlateMortgageCalc300 border-r-0 rounded-s-md">
                   <span className='font-bold text-SlateMortgageCalc700'>£</span>
                 </span>
-                <input type="text" id="website-admin" className="rounded-none rounded-e-lg bg-gray-50 border border-l-0 text-gray-900  w-full text-sm border-SlateMortgageCalc300 p-2.5 h-12" />
+                <input type="text" id="website-admin" className="rounded-none rounded-e-lg bg-gray-50 border border-l-0 text-gray-900  w-full text-base font-bold border-SlateMortgageCalc300 p-2.5 h-12" />
               </div>
 
               <div className='flex gap-7 mb-5'>
                 <div>
                   <label htmlFor="log-email" className="text-SlateMortgageCalc700 font-medium text-sm">Mortgage Term</label>
                   <div class="flex mt-2">
-                    <input type="text" id="website-admin" className="rounded-l-lg bg-gray-50 border border-r-0 text-gray-900 text-sm border-SlateMortgageCalc300 p-2.5 h-12" />
+                    <input type="text" id="website-admin" className="rounded-l-lg bg-gray-50 border border-r-0 text-gray-900  border-SlateMortgageCalc300 p-2.5 h-12 text-base font-bold focus:border-r-0" />
 
                     <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-SlateMortgageCalc100 border rounded-e-0 border-SlateMortgageCalc300 border-l-0 rounded-r-lg">
                       <span className='font-bold text-SlateMortgageCalc700'>years</span>
@@ -38,7 +38,7 @@ const MortgageCalc = () => {
                 <div>
                   <label htmlFor="log-email" className="text-SlateMortgageCalc700 font-medium text-sm">Interest Rate</label>
                   <div class="flex mt-2">
-                    <input type="text" id="website-admin" className="rounded-l-lg bg-gray-50 border border-r-0 text-gray-900 text-sm border-SlateMortgageCalc300 p-2.5 h-12" />
+                    <input type="text" id="website-admin" className="rounded-l-lg bg-gray-50 border border-r-0 text-gray-900 text-sm font-bold border-SlateMortgageCalc300 p-2.5 h-12" />
 
                     <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-SlateMortgageCalc100 border rounded-e-0 border-SlateMortgageCalc300 border-l-0 rounded-r-lg">
                       <span className='font-bold text-SlateMortgageCalc700'>%</span>
@@ -73,32 +73,33 @@ const MortgageCalc = () => {
 
         <div className='w-300 bg-SlateMortgageCalc900 rounded-tr-4xl rounded-bl-[120px] rounded-br-4xl'>
 
-          <div className='result p-12'>
-            <h1 className='text-WhiteMortgageCalc font-bold text-xl mb-5'>Your Results</h1>
-            <p className='text-SlateMortgageCalc700'>Your results are shown below based on the information you provided. To adjust the result
-              edit the form and click "Calculate repayments" again.
-            </p>
-
-            <div className='border-t-4 mt-10 rounded-md border-LimeMortgageCalc bg-SlateMortgageCalc600 h-70 p-10 flex flex-col justify-between'>
-              <div className='border-b-1 pb-9 border-SlateMortgageCalc300'>
-                <h2 className='text-SlateMortgageCalc700'>Your monthly repayments</h2>
-                <h1 className='text-LimeMortgageCalc font-semibold text-5xl'><span>£</span>1,797.974</h1>
-              </div>
-
-              <div>
-                <p className='text-SlateMortgageCalc700'>Total you'll repay over the term</p>
-                <h2 className='text-WhiteMortgageCalc font-bold text-xl'><span>£</span>539,322.94</h2>
-              </div>
-            </div>  
-          </div>
-
-          <div className='hidden items-center flex-col justify-center h-full'>
+          {/* Default State */}
+          <div className='flex items-center flex-col justify-center h-full'>
             <img src={illustratioon} alt="" className='w-50' />
 
             <h1 className='text-WhiteMortgageCalc font-bold text-[22px]'>Results shown here</h1>
             <p className='text-SlateMortgageCalc300 text-center text-md px-16 pt-3'>Complete the form and click "calculate repayments" to see what your monthly repayments would be. </p>
           </div>
 
+          {/* After result */}
+          <div className='hidden result p-12'>
+            <h1 className='text-WhiteMortgageCalc font-bold text-xl mb-5'>Your Results</h1>
+            <p className='text-SlateMortgageCalc700 text-sm'>Your results are shown below based on the information you provided. To adjust the result
+              edit the form and click "Calculate repayments" again.
+            </p>
+
+            <div className='border-t-4 mt-10 rounded-md border-LimeMortgageCalc bg-SlateMortgageCalc600 h-70 p-10 flex flex-col justify-between'>
+              <div className='border-b-1 pb-9 border-SlateMortgageCalc300'>
+                <h2 className='text-SlateMortgageCalc700'>Your monthly repayments</h2>
+                <h1 className='text-LimeMortgageCalc font-semibold text-5xl mt-2'><span>£</span>1,797.974</h1>
+              </div>
+
+              <div>
+                <p className='text-SlateMortgageCalc700 mb-2'>Total you'll repay over the term</p>
+                <h2 className='text-WhiteMortgageCalc font-bold text-xl'><span>£</span>539,322.94</h2>
+              </div>
+            </div>
+          </div>
 
         </div>
       </div>
